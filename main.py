4e3868965ab3939f6ddf7f4d83ff3f7adf1c830d
1138971 <1138971@atitus.edu.br>
2025-10-31 13:14:41 -0300

    importado bibliotecas e adicionando as funções: pausa, limpar terminal e menu

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from funcoes import (
+    garantir_arquivos, carregar_produtos, cadastrar_produto,
+    listar_produtos, editar_produto, excluir_produto, log_action
+)
+
+import os
+import sys
+import time
+
+
+def limpar_tela():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def pausa():
+    input("\nPressione Enter para continuar...")
+
+def menu_principal():
+    garantir_arquivos()
+    produtos = carregar_produtos()
 
